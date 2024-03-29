f9fbec9a3c8e8b18deab9ea60a6f8222ef06b873
himanshu1161 <mishrah.11603@gmail.com>
2024-03-29 17:01:02 +0530

    Auto carousel in testimonial

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { NewsModal } from "./ContentModal";
 
 const Testimonials = ({ data }) => {
@@ -9,6 +9,8 @@ const Testimonials = ({ data }) => {
     title: "",
     date: "",
   });
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [animate, setAnimate] = useState(true); 
 
   const onOpenModal = (title, date) => {
     setOpen(true);
@@ -19,6 +21,18 @@ const Testimonials = ({ data }) => {
     setOpen(false);
     setModalValue({ img: null, title: "", date: "" });
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAnimate(true);
+      setTimeout(() => {
+        const nextIndex = (currentIndex + 1) % testimonials.length;
+        setCurrentIndex(nextIndex);
+        setAnimate(false); 
+      }, 500); 
+    }, 7000);
+    return () => clearInterval(interval);
+  }, [currentIndex, testimonials.length]);
 
   return (
     <Fragment>
@@ -33,21 +47,21 @@ const Testimonials = ({ data }) => {
       {/* Testimonials */}
       <div className="kura_tm_section" id="testimonials">
         <div className="kura_tm_testimonials">
-          <div className="container" style={{marginBottom:"6rem"}}>
+          <div className="container" style={{ marginBottom: "6rem" }}>
             <div className="kura_tm_main_title">
               <span className="head_test">Testimonials</span>
               <h3 className="h_t3">What People Say</h3>
             </div>
-            <div className="testimonials_list">
-              {testimonials.map((testimonial, index) => (
-                <div key={index} className="testimonial_box">
-                  <div className="inner">
-                  <h5 className="h_t2">{testimonial.name} - {testimonial.position}</h5>
-                    <p className="h_t2">{testimonial.review.replace(/"/g, "")}</p>
-                   <hr />
-                  </div>
+            <div className="testimonial_box">
+              <div className="inner" style={{ animation: animate ? "fadeInOut 1s ease-in-out" : "none" }}>
+                <div>
+                  <h5 className="h_t2">
+                    {testimonials[currentIndex].name} - {testimonials[currentIndex].position}
+                  </h5>
+                  <p className="h_t2">{testimonials[currentIndex].review.replace(/"/g, "")}</p>
+                  <hr />
                 </div>
-              ))}
+              </div>
             </div>
           </div>
         </div>
