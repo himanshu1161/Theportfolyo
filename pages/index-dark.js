62f94aa88a53ab26a75c286c7e289bde7781a4af
himanshu1161 <mishrah.11603@gmail.com>
2024-03-29 14:41:10 +0530

    minor changes

--- pages/index-dark.js
+++ pages/index-dark.js
@@ -29,7 +29,7 @@ const IndexDark = () => {
           <Skills />
           <Timeline />
           <Price />
-          <Testimonials />
+          {/* <Testimonials /> */}
           <Contact />
           <Footer />
           <Audio />
